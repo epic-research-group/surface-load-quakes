a5be0f69d9bda8852ec13dbd97372c87ffc5cd04
Amanda Syamsul <amand4@sermeq.ess.washington.edu>
2022-01-18 22:48:25 -0500

    Analysis on mainshocks (mizrahi)

---
+++ loadquakes.py
@@ -0,0 +1,72 @@
+import xarray as xr
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from global_land_mask import globe
+import scipy.stats as stats
+import os
+
+def plot_hist(all_time_periods, earthquake_only, ax1, ax2, title1, title2):
+    
+    # Cumulative histogram
+
+    bins = np.linspace(-80,80,41)
+    ax1.hist(earthquake_only, bins, density = True, cumulative=True, histtype='step',
+            label='Time periods with an earthquake',linewidth=1.5)
+    ax1.hist(all_time_periods, bins, density = True, cumulative=True,histtype='step',
+            label='All time periods',linewidth=1.5)
+    ax1.set_ylim((-0.1,1.3))
+    ax1.legend()
+    ax1.set_xlabel('Surface load (cm water equiv.)', fontsize = 17)
+    ax1.set_ylabel("Cumulative probability", fontsize = 17)
+    ax1.set_title(title1)
+    
+    # Non-cumulative histogram
+
+    ax2.hist(earthquake_only, bins, density = True, cumulative=False, histtype='step',
+            label='Time periods with an earthquake',linewidth=1.5)
+    ax2.hist(all_time_periods, bins, density = True, cumulative=False,histtype='step',
+            label='All time periods',linewidth=1.5)
+    yl = ax2.get_ylim()
+    ax2.set_ylim((-0.01,1.4*yl[1]))
+    ax2.set_xlim((-40,60))
+    ax2.legend()
+    ax2.set_xlabel('Surface load (cm water equiv.)', fontsize = 17)
+    ax2.set_ylabel("Probability", fontsize = 17)
+    ax2.set_title(title2)
+    
+def plot_rel_hist(all_time_periods, earthquake_only, ax, title):
+    
+    bins = np.linspace(-80,80,41)
+    LgE = np.histogram(earthquake_only, bins=bins, density = True)[0]
+    L   = np.histogram(all_time_periods,bins=bins, density = True)[0]
+
+    wid = np.mean(np.diff(bins))
+    ax.bar(bins[:-1]+wid/2,LgE/L,width=wid)
+
+    ax.plot([-80,80],[1, 1],'--r')
+    ax.text(-20, 1.5,'P=P(E)',color='r',fontsize=20)
+    ax.set_xlabel('Surface Load (cm water equiv.)',fontsize = 17)
+    ax.set_ylabel('Relative Probability',fontsize = 17)
+    ax.set_title(title, fontsize = 17)
+    #return fig,ax
+    
+def calc_stats(a,b):
+    '''
+    Calculate stats for the distributions a and b
+    a: distribution during earthquakes
+    b: distribution over all time periods
+    '''
+    
+    result = {} # this creates a dictionary
+    
+    result['cvm'] = stats.cramervonmises_2samp(a, b, method='auto')
+    result['ks'] = stats.ks_2samp(a, b)
+    result['median_all'] = np.median(b)
+    result['median_eq'] = np.median(a)
+    result['mean_all'] = np.mean(b)
+    result['mean_eq'] = np.mean(a)
+    result['mean_all_minus_mean_eq'] = np.mean(b)-np.mean(a)
+    result['median_all_minus_median_eq'] = np.median(b)-np.median(a)
+    
+    return result
