25dfad4c35485fba6502b95ac711b7c252843462
Amanda Syamsul <amand4@sermeq.ess.washington.edu>
2022-01-19 19:34:09 -0500

    sorting mainshocks by load/magnitude

--- loadquakes.py
+++ loadquakes.py
@@ -37,7 +37,7 @@ def plot_hist(all_time_periods, earthquake_only, ax1, ax2, title1, title2):
     
 def plot_rel_hist(all_time_periods, earthquake_only, ax, title):
     
-    bins = np.linspace(-80,80,41)
+    bins = np.linspace(-80,80,int(1 + 3.322*np.log(earthquake_only.size)))
     LgE = np.histogram(earthquake_only, bins=bins, density = True)[0]
     L   = np.histogram(all_time_periods,bins=bins, density = True)[0]
 
@@ -73,17 +73,8 @@ def calc_stats(a,b):
 
 # function to plot maps of earthquake distribution
 
-def plot_map(eq, load_bounds, label1):
 
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    ax = world.plot(color='white', edgecolor='black', figsize=(15,10))
-
-    # first PC
-    gdf = gpd.GeoDataFrame(load_bounds,
-                       geometry=gpd.points_from_xy(load_bounds.longitude, load_bounds.latitude))
-    gdf.plot(ax=ax, label=label1)
     
-    leg = ax.legend()
-    ax.set_xlabel('Longitude', fontsize = 15)
-    ax.set_ylabel("Latitude", fontsize = 15)
-    plt.show()
+
+
+   
